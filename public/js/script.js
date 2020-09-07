21b5799b472f2dd9c20fc181e4f43f3e9a815f2a
misano <misanolukabwe@gmail.com>
2020-09-07 10:44:09 +0200

    yeup

---
+++ public/js/script.js
@@ -0,0 +1,94 @@
+// var chiffre1 = "cinq"
+// var chiffre2 = 5
+// if (5 === cinq) {
+//     console.log();
+// }
+
+// 4°
+// let monArray = []
+
+// 5°
+// let monobjet ={
+// ustensil : "couteau",
+// }
+
+// 6°/////////////
+
+// 7°
+// var objet= {
+//     nom : "misano",
+//     methode(){
+//         this.age = prompt("changer son age")
+//     },
+
+// }
+// objet.methode();
+
+// // 1°ere oldschool
+// // alert(objet.nom, +"a"+ objet.age + "ans");
+// console.log(`${objet.nom} a,${objet.age} ans`);
+
+// 8°
+
+
+// const logIn = () => {
+//     do {
+//         var mdp = prompt("Entrez un mot de passe");
+//         mdp = mdp.toLowerCase();
+//         if (mdp === 'mdp') {
+//             alert("Vous êtes connectés")
+//         } else {
+//             alert("Mauvais mot de passe")
+//         }
+//     } while (mdp !== 'mdp');
+//     return mdp;
+// }
+// console.log(logIn());
+
+// 9°
+// let monInt
+
+// let monInt = prompt ("mettez votre age");
+
+// if(monInt < 8) {
+//     console.log("désolé, vous etes mineur")
+// } else {
+//     console.log("bienvenu")
+// }
+
+
+// 10°
+
+// let nombre = +prompt('Un nombre ?')
+// let tab = []
+// for (let i = 0; i < nombre; i++) {
+//     tab.push(i);
+//     console.log('let');
+// }
+
+// console.log(tab);
+
+// 10°
+
+// 11°
+// let datum = new Date();
+
+// console.log(datum.toLocaleDateString ())
+
+//  12°
+
+
+// let monobjet ={
+// ustensil : "couteau",
+
+// console.log(ustensil.toUppercase());
+// }
+
+
+// 13°
+// let maj = prompt ("quel est ton nom ?");
+
+// console.log(maj.toUpperCase());
+
+
+let phrase = prompt ("comment tu te sens ?")
